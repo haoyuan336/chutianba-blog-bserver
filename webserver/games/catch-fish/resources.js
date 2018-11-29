7a337f8d6f637a0d930b62291c04fccaea323ac1
楚浩远 <>
2018-11-29 17:14:52 +0800

    添加资源

--- webserver/games/catch-fish/resources.js
+++ webserver/games/catch-fish/resources.js
@@ -10,6 +10,13 @@ const res =
   "json_fish_path_7": "./games/catch-fish/images/fish_path_7.json",
   "json_fish_path_8": "./games/catch-fish/images/fish_path_8.json",
   "game_bg": "./games/catch-fish/images/game_bg.jpg",
+  "paodan1": "./games/catch-fish/images/paodan1.png",
+  "paodan2": "./games/catch-fish/images/paodan2.png",
+  "paodan3": "./games/catch-fish/images/paodan3.png",
+  "paodan4": "./games/catch-fish/images/paodan4.png",
+  "paodan5": "./games/catch-fish/images/paodan5.png",
+  "paodan6": "./games/catch-fish/images/paodan6.png",
+  "paodan7": "./games/catch-fish/images/paodan7.png",
   "json_texturepacker": "./games/catch-fish/images/texturepacker.json",
   "texturespack": "./games/catch-fish/images/texturespack.png"
 };
